dd3aef4803186817cda43a5a4ead940f69a1b794
gusosb <83115021+gusosb@users.noreply.github.com>
2022-06-14 14:12:28 +0200

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,75 +1,54 @@
 import './Styles.css'
 import { Route, Routes, useLocation } from "react-router-dom"
 import CssBaseline from '@mui/material/CssBaseline'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Suspense, lazy, useEffect } from 'react'
-import FiraSansWoff2 from './WOFF2/FiraSans-Regular.woff2'
-import ReactGA from "react-ga4"
 
-
-
-ReactGA.initialize("G-T7J1QQHEN8")
-
-const Home = lazy(() => import('./components/Home'))
-const GetStarted = lazy(() => import('./components/GetStarted'))
-
-
-const Base = lazy(() => import('./components/Base'))
-const AscendBkContent = lazy(() => import('./components/AscendBkContent'))
-const AscendTaxContent = lazy(() => import('./components/AscendTaxContent'))
-const RemoteSupport = lazy(() => import('./components/RemoteSupport'))
-
-const FAQPage = lazy(() => import('./components/FAQPage'))
 const Flex = lazy(() => import('./components/Flex/Flex'))
 
 
 
 const App = () => {
 
-  const location = useLocation()
+  document.title = "wkFlex"
+
+  const psw = sessionStorage.getItem('psw')
+  const pass = 'wk'
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+
+  const client = useApolloClient()
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
 
-  
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+
   useEffect(() => {
-    if (!window.location.hostname === "localhost") {
-      ReactGA.send({ hitType: "pageview", page: location.pathname })
+    if (!token) {
+      // get token from localstorage, if available and set it to token
+      const localtoken = localStorage.getItem('wkflextoken')
+      if(localtoken) {
+        setToken(localtoken)
+      }
     }
-  }, [location])
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#007AC3"
-      },  
-      secondary: {
-        main: "#EDEDED"
-      },
-      green: {
-        main: "#85BC20"
-      },
-      red: {
-        main: "#E5202E"
-      },
-    },
-    typography: {
-      fontFamily: 'Raleway, Arial',
-    },
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
-          @font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-display: swap;
-            font-weight: 400;
-            src: local('Raleway'), local('Raleway-Regular'), url(${FiraSansWoff2}) format('woff2');
-            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-          }
-        `,
-      },
-    },
-  })
-
+    if (psw !== pass) {
+      sessionStorage.setItem('psw', prompt("Please enter the password."))
+      window.location.reload()
+      }
+  }, [token, psw])
 
   
   return (
@@ -77,25 +56,14 @@ const App = () => {
     
     
     <Suspense fallback={<></>}>
-    <ThemeProvider theme={theme}>
-    <CssBaseline />
       <Routes>
-        
-        <Route path="capego" element={<Base />}>
-        <Route index element={(<Home />)} />
-          <Route path="overgang-bokslut" element={<AscendBkContent />} />
-          <Route path="overgang-skatt" element={<AscendTaxContent />} />
-          <Route path="overgang-faq" element={<FAQPage />} />
-          <Route path="fjarrsupport" element={<RemoteSupport />} />
-        </Route>
+      <Route path="login" element={!token ? <FlexLogin notify={notify} errorMessage={errorMessage} setToken={setToken} /> : <Navigate replace to="/internal/wkflex/" />} />
+      <Route path="register" element={!token ? <FlexRegister notify={notify} errorMessage={errorMessage} setToken={setToken} /> : <Navigate replace to="/internal/wkflex/" />} />
 
-        <Route path="/kom-igang-capego/" element={<GetStarted />} />
-        <Route path="/internal/wkflex/*" element={<Flex />} />
-        <Route path="/" element={<Home />} />
+      <Route path="admin" element={<FlexAdmin token={token} logout={logout} />} />
+      <Route path="/" element={token ? <FlexHome logout={logout} /> : <Navigate replace to="login" />} />
 
-        
       </Routes>
-      </ThemeProvider>
       </Suspense>
     </>
   )
--- frontend/src/helpers/history.js
+++
@@ -1,6 +0,0 @@
-import { createBrowserHistory } from "history"
-import { useNavigate } from 'react-router-dom'
-
-//export const history = createBrowserHistory()
-
-export const history = useNavigate()
